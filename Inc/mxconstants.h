c944146ffb9e6c88aab04f1c2f5e12ed4fb7a76e
iamhyc <sudofree@163.com>
2015-12-29 10:10:48 +0800

    DN about PWM!!

---
+++ Inc/mxconstants.h
@@ -0,0 +1,83 @@
+/**
+  ******************************************************************************
+  * File Name          : mxconstants.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2015 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define FIST_IN_Pin GPIO_PIN_0
+#define FIST_IN_GPIO_Port GPIOA
+#define DM_IN_Pin GPIO_PIN_1
+#define DM_IN_GPIO_Port GPIOA
+#define ROLL_IN_Pin GPIO_PIN_2
+#define ROLL_IN_GPIO_Port GPIOA
+#define YAW_IN_Pin GPIO_PIN_3
+#define YAW_IN_GPIO_Port GPIOA
+#define nPinM_Pin GPIO_PIN_4
+#define nPinM_GPIO_Port GPIOA
+#define pPinM_Pin GPIO_PIN_5
+#define pPinM_GPIO_Port GPIOA
+#define DM_OUT_Pin GPIO_PIN_6
+#define DM_OUT_GPIO_Port GPIOA
+#define ROLL_OUT_Pin GPIO_PIN_7
+#define ROLL_OUT_GPIO_Port GPIOA
+#define YAW_OUT_Pin GPIO_PIN_0
+#define YAW_OUT_GPIO_Port GPIOB
+#define AutoSw_Pin GPIO_PIN_1
+#define AutoSw_GPIO_Port GPIOB
+#define FIST_OUT_Pin GPIO_PIN_2
+#define FIST_OUT_GPIO_Port GPIOB
+#define nPinR_Pin GPIO_PIN_14
+#define nPinR_GPIO_Port GPIOA
+#define pPinR_Pin GPIO_PIN_15
+#define pPinR_GPIO_Port GPIOA
+#define R_Wheel_Pin GPIO_PIN_8
+#define R_Wheel_GPIO_Port GPIOB
+#define L_Wheel_Pin GPIO_PIN_9
+#define L_Wheel_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
